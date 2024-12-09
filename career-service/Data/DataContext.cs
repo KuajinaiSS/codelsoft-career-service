feb02da164cda50830ac5bcb1de7a73a59e55b8d
KuajinaiSS <kuajinaiss@gmail.com>
2024-12-09 00:32:09 -0300

    feature: created the Models for Subject.cs and SubjectRelationship.cs, aan added in the DataContext

--- career-service/Data/DataContext.cs
+++ career-service/Data/DataContext.cs
@@ -6,6 +6,8 @@ namespace career_service.Data;
 public class DataContext : DbContext
 {
     public DbSet<Career> Careers { get; set; } = null!;
+    public DbSet<Subject> Subjects { get; set; } = null!;
+    public DbSet<SubjectRelationship> SubjectsRelationships { get; set; } = null!;
     
     public DataContext(DbContextOptions options) : base(options) { }
     
---
+++ career-service/Models/SubjectRelationship.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace career_service.Models;
+
+public class SubjectRelationship : BaseModel
+{
+    [StringLength(250)]
+    public string SubjectCode { get; set; } = null!;
+
+    [StringLength(250)]
+    public string PreSubjectCode { get; set; } = null!;
+    
+    
+}
